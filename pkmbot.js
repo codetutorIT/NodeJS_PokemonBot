4256d46e6eeb2b76a4bf910bff6597a414af3f8d
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2020-02-16 15:44:25 +0100

    Initial commit

---
+++ pkmbot.js
@@ -0,0 +1,45 @@
+const TelegramBot = require('node-telegram-bot-api');
+const Discord = require('discord.js');
+const Scheduler = require('node-schedule');
+const config = require("./config.json");
+
+const discord = new Discord.Client();
+const telegram = new TelegramBot(config.telegram_token, {polling: true});
+
+
+
+discord.on('message', message => {
+    if(message.isMentioned(discord.users.get(config.discord_userid))){
+        var discord_message = message.toString();
+        var prefix = '<@';
+        var prefix_alt = '<@!';
+        var suffix = '>';
+        var search_string = prefix.concat(config.discord_userid, suffix);
+        var msg = discord_message.replace(search_string, discord.users.get(config.discord_userid).username);
+        search_string = prefix_alt.concat(config.discord_userid, suffix);
+        msg = discord_message.replace(search_string, discord.users.get(config.discord_userid).username);
+        console.log('[PKMBOT] ' + msg);
+        telegram.sendMessage(config.telegram_chatid, msg);   
+    }
+});
+
+discord.on('ready', () => {
+    console.log('[PKMBOT] I\'m ready!');
+    var rollScheduler = Scheduler.scheduleJob('1 */2 * * *', getPokemon);
+})
+
+
+discord.login(config.discord_token);
+
+function getPokemon(){
+    console.log('[PKMBOT] Rolling for Pokemons!');
+    var guild = discord.guilds.get(config.discord_guildid);
+    if(guild && guild.channels.get(config.discord_channelid)){
+        guild.channels.get(config.discord_channelid).send("$pokemon");
+        console.log('[PKMBOT] Successfully rolled for Pokemon!');
+        telegram.sendMessage(config.telegram_chatid, "[PKMBOT] Successfully rolled for Pokemon!");
+    } else {
+        console.log("[PKMBOT] ERROR");
+        telegram.sendMessage(config.telegram_chatid, "[PKMBOT] ERROR!");
+    }
+}
